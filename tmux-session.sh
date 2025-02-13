8deef3a1fa8013869b4ca4d7c127265292ef2fe7
jaccoobi <jacobgundlach13@outlook.com>
2025-02-12 21:32:31 -0500

    Opening project works
    This took waaaaaay too long

--- tmux-session.sh
+++ tmux-session.sh
@@ -6,8 +6,24 @@ read -p ": " option
 if [ $option == "c" ]; then
   echo "TEST: creating new project"
 elif [ $option == "o" ]; then
-  project_dir=`cat .projects | fzf`
-  echo $project_dir
+  project_dir=`find ~/Documents/dev -maxdepth 1 | fzf`
+
+  project_name=`basename $project_dir | tr . _`
+  tmux rename-session $project_name
+
+  cd $project_dir
+  tmux rename-window "nvim"
+
+  tmux new-window
+  tmux rename-window "run"
+  
+  tmux new-window
+  tmux rename-window "git"
+
+  # Go to first window
+  tmux select-window -t:nvim
+  cd $project_dir
+  yazi
 else
   echo "Not a valid command."
 fi
