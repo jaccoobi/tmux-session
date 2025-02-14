290a684542e44ce45b3cc353ca5bef03c8bb291f
jaccoobi <jacobgundlach13@outlook.com>
2025-02-13 20:59:58 -0500

    implemented creating projects

--- tmux-session.sh
+++ tmux-session.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/bash
 
-read -p "Create new project or open an existing one? " option
-
-if [ $option == "c" ]; then
-  echo "TEST: creating new project" #TODO
-elif [ $option == "o" ]; then
-  project_dir=`find ~/Documents/dev -mindepth 1 -maxdepth 1 | fzf`
-  cd $project_dir
-
-  project_name=`basename $project_dir | tr . _`
+open_session() {
+  project_name=`basename $1 | tr . _`
   tmux rename-session $project_name
 
   tmux rename-window "nvim"
@@ -20,6 +13,22 @@ elif [ $option == "o" ]; then
   # Go to first window
   tmux select-window -t:nvim
   yazi
+}
+
+read -p "Create new project or open an existing one? " option
+
+if [ $option == "c" ]; then
+  read -p "What is the name of the project? " name
+  cd ~/Documents/dev/
+  mkdir $name
+  cd $name
+
+  open_session ~/Documents/dev/$project_dir
+elif [ $option == "o" ]; then
+  project_dir=`find ~/Documents/dev -mindepth 1 -maxdepth 1 | fzf`
+  cd $project_dir
+
+  open_session $project_dir
 elif [ $option == "q" ]; then
   exit 0 # TODO
 else
