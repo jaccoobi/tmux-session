7178f094c9a37cab27ad73571ca97926b9f3dd86
jaccoobi <jacobgundlach13@outlook.com>
2025-02-13 19:47:25 -0500

    idek what i did

--- tmux-session.sh
+++ tmux-session.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 
-echo "Create new project or open an existing one?"
-read -p ": " option
+read -p "Create new project or open an existing one? " option
 
 if [ $option == "c" ]; then
-  echo "TEST: creating new project"
+  echo "TEST: creating new project" #TODO
 elif [ $option == "o" ]; then
-  project_dir=`find ~/Documents/dev -maxdepth 1 | fzf`
+  project_dir=`find ~/Documents/dev -mindepth 1 -maxdepth 1 | fzf`
 
   project_name=`basename $project_dir | tr . _`
   tmux rename-session $project_name
@@ -19,13 +18,13 @@ elif [ $option == "o" ]; then
   
   tmux new-window
   tmux rename-window "git"
+  bash lazygit
 
   # Go to first window
   tmux select-window -t:nvim
-  cd $project_dir
   yazi
 elif [ $option == "q" ]; then
-  exit 0
+  exit 0 # TODO
 else
   echo "Not a valid command."
 fi
