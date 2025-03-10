fc8f5656e90121a017a4bd398b5af04b5f1d3566
jaccoobi <jacobgundlach13@outlook.com>
2025-03-09 21:03:39 -0400

    Added maven implementation
    The maven archetype now works. All that is needed is a .gitignore file
    (which can be copied from github)

--- tmux-session.sh
+++ tmux-session.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/bash
 
 java_project() {
+  read -p "Enter the groupId (org.apache.maven): " group_id
+  read -p "Enter the artifactID (maven-clean-plugin): " artifact_id
+
   # maven
-  mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5
+  mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId="$group_id" -DartifactId="$artifact_id"
+
+  # rename dir to language standard
+  mv "./$artifact_id/" "./java-$artifact_id/"
+  cd "java-$artifact_id"
 
   # git
   git init
+
+  open_session "~/Documents/dev/java-$artifact_id"
 }
 
 open_session() {
@@ -24,19 +33,20 @@ open_session() {
 read -p "Create new project or open an existing one? " option
 
 if [ $option == "c" ]; then
-  read -p "What is the name of the project? " name
   languages="java"
-  project_language=`echo $languages | fzf`
+  project_language=`echo $languages | fzf --prompt "Language > "`
 
   cd ~/Documents/dev/
-  mkdir $name
-  cd $name
 
   if [ $project_language == "java" ]; then
     java_project
+  else
+    read -p "What is the name of the project? " name
+    mkdir $name
+    cd $name
+    open_session ~/Documents/dev/$name
   fi
 
-  open_session ~/Documents/dev/$name
 elif [ $option == "o" ]; then
   project_dir=`find ~/Documents/dev -mindepth 1 -maxdepth 1 | fzf`
   cd $project_dir
